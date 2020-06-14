4bd47c6ee1678bcc4b5222798c4a44cd788df8f2
Nicole Antonacci <nicole.antonacci@gmail.com>
2020-06-14 11:02:32 -0700

    moving zeros FP solved.

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -4,12 +4,41 @@ Returns: a List of integers
 '''
 def moving_zeroes(arr):
     # Your code here
+    zeros = []
+    popcount = 0
+    n = 0
+    zerocount = 0
+    arrlength = len(arr)
+    print(arrlength)
+    for i in arr:
+        if i == 0:
+            zerocount+=1
+            print(zerocount)
+        else:
+            pass
+    
+    if zerocount == arrlength:
+        pass
 
-    pass
+    else:
+        while n < len(arr)-popcount:
+            print(arr)
+            if arr[n] != 0:
+                print(arr)
+                n+=1
+            elif arr[n] == 0:
+                print(arr)
+                zeros.append(arr[n])
+                arr.pop(n)
+                popcount+=1
+                n-=1
+
+    arr+=zeros
+    return arr
 
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
-    arr = [0, 3, 1, 0, -2]
+    arr = [0, 0, 0, 0, 3, 2, 1] 
 
     print(f"The resulting of moving_zeroes is: {moving_zeroes(arr)}")
