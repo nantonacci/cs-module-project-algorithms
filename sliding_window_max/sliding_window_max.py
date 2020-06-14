a2b6fa81d13e7f63df4ba877183542294f919c05
Nicole Antonacci <nicole.antonacci@gmail.com>
2020-06-14 12:13:12 -0700

    FP sliding window complete.

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -2,10 +2,26 @@
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
 '''
+
+#  a[start:stop]  # items start through stop-1
+
 def sliding_window_max(nums, k):
     # Your code here
-
-    pass
+# window arr of k elements
+# slice
+# moves one at a time - [1,2,3], [2,3,4], [3,4,5]
+# get highest number for each slice - 3, 4, 5
+# return arr of highest numbers - [3, 4, 5]
+    arrlen = len(nums)
+    # increment slice?
+    start = 0
+    stop = k
+    highest = []
+    while stop <= arrlen:
+        highest.append(max(nums[start:stop]))
+        start+=1
+        stop+=1
+    return highest
 
 
 if __name__ == '__main__':
